557142b0c95ffee6ab7792077b41e7d67850af9f
Ing. Luis Ramirez <mr2002gl@gmail.com>
2025-02-07 12:51:32 -0500

    Sigo practicando java Script

--- clase-conversion-de-tipos.js
+++ clase-conversion-de-tipos.js
@@ -19,3 +19,29 @@ console.log(typeof decimal);
 
 const suma = '5' + 3
 console.log(suma);
+
+
+const sumWithBoolean = '3' + true
+console.log(sumWithBoolean);
+
+
+const sumWithNumber = 2 + true
+console.log(sumWithNumber);
+
+
+const stringValue = '10'
+const numberValue = 10
+const booleanValue = true
+
+console.log(stringValue + numberValue);
+console.log(stringValue + booleanValue);
+console.log(stringValue + stringValue);
+console.log(numberValue + stringValue);
+console.log(numberValue + booleanValue);
+console.log(numberValue + numberValue);
+console.log(booleanValue + stringValue);
+console.log(booleanValue + numberValue);
+console.log(booleanValue + booleanValue);
+
+
+
