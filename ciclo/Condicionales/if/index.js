a33ab3f3417049d263f056f7c48378474e045e29
Ing. Luis Ramirez <mr2002gl@gmail.com>
2025-02-07 15:24:41 -0500

    Aprendi if else if y else

---
+++ ciclo/Condicionales/if/index.js
@@ -0,0 +1,9 @@
+let nombre = "Diego"
+
+if(nombre === "Diego"){
+    console.log('Bienvenido: ' + nombre);
+}else if (nombre === "Nico"){
+    console.log("Bienvenido: " + nombre);
+}else{
+    console.log("Nombre no encontrado");
+}
