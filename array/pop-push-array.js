1269b2ef19a0261b1a050b4b871ac848e3d09687
Ing. Luis ramirez <lrm20023@gmail.com>
2025-02-13 21:00:52 -0500

    Aprendiendo push y pop

---
+++ array/pop-push-array.js
@@ -0,0 +1,15 @@
+// Metodos que modifican el array original mutabilidad
+
+// push
+
+const paises = ['Colomabia', 'Canada', 'China']
+const newPaises = paises.push('Argentina', 'Peru')
+console.log(paises);
+console.log(newPaises);
+
+//pop
+
+const removedPaises = paises.pop()
+
+console.log(paises);
+console.log(removedPaises);
